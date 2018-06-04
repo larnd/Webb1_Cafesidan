fb0867b16ea56882314a3dd777e8cffa79d04901
Andreas Larsson <andreas.larsson@jonkoping.se>
2018-06-04 08:28:41 +0200

    Lagt till interna länkar, centrerat rubrik, justerat höjd

--- js/test.js
+++ js/test.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+  var topposition = $(this).offset();
   $('.menuButton').click(function(){
-    $('.meny').animate({'left':'0px'},1000);
+    $('.meny').animate({'left':'0px'},250);
   });
 
   $('.closeButton').click(function(){
-    $('.meny').animate({'left':'-250px'},1000);
+    $('.meny').animate({'left':'-250px'},250);
   });
+
 });
